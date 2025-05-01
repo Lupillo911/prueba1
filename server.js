e37a66942f0d033b4ed5ed8bf419842ba990feed
Lupillo <joselupillo2603@gmail.com>
2025-04-30 19:31:42 -0600

    Agregar ruta raíz

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // Configura tu conexión a Turso
 const db = createClient({
   url: 'libsql://prueba1-lupillo911.aws-us-east-1.turso.io',
-  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDYwNjA3NTMsImlkIjoiMzVlNTk2YTUtZjUyYy00MmY5LTgxZGQtN2VhMTY3ZTEyOGI3IiwicmlkIjoiYjQ4NzRkNGItMzNhMS00NGJlLTg3OWUtZDQ3YzIzYWE4NDY1In0.dVq6MT-EaZWh_r2v4UGNJ271_XOT9AbwCL5uWi7rkgD7TH1rEH3MBNJ2LfWpwvo9KJqZKILkKviD1wIQ0yCiDw'
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDYwNjMwNTMsImlkIjoiMzVlNTk2YTUtZjUyYy00MmY5LTgxZGQtN2VhMTY3ZTEyOGI3IiwicmlkIjoiYjQ4NzRkNGItMzNhMS00NGJlLTg3OWUtZDQ3YzIzYWE4NDY1In0.OI1jvbWvTfcQW4D2zzPpAsQfwcIFaiSPnSrPyrgCxQZ2wGgw-nsK2XuqacHcUnHDqxx2Xftd0DmXKZATtXxADg'
 });
 
 // Ruta raíz para comprobar si el servidor funciona
